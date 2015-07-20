85ff602e33b302c3caa6d21519144aa3671ee75b
David Liman <limanoit@gmail.com>
2015-07-19 18:33:38 -0700

    first pass, add hackernews and reddit scoring

--- hotscore.go
+++ hotscore.go
@@ -1,6 +1,9 @@
 package hotscore
 
-import "math"
+import (
+        "math"
+        "time"
+)
 
 // wilson score interval sort
 // http://www.evanmiller.org/how-not-to-sort-by-average-rating.html
@@ -18,4 +21,21 @@ func Wilson(ups, downs int) float64 {
 	zzfn := z * z / (4 * n1)
 
 	return (p + 2.0 * zzfn - z * math.Sqrt((zzfn / n1 + p * (1.0 - p)) / n1)) / (1 + 4 * zzfn)
+}
+
+// hackernews' hot sort
+// http://amix.dk/blog/post/19574
+func Hacker(votes int, date time.Time) float64 {
+        gravity := 1.8
+        hoursAge := float64(date.Unix() * 3600)
+        return float64(votes - 1) / math.Pow(hoursAge + 2, gravity)
+}
+
+// reddit's hot sort
+// http://amix.dk/blog/post/19588
+func Reddit(ups int, downs int, date time.Time) float64 {
+        decay := int64(45000)
+        s := float64(ups - downs)
+        order := math.Log(math.Max(math.Abs(s), 1)) / math.Ln10
+        return order - float64(date.Unix() / decay)
 }
--- hotscore_test.go
+++ hotscore_test.go
@@ -4,3 +4,9 @@ import "testing"
 
 func TestWilson(t *testing.T) {
 }
+
+func TestHacker(t *testing.T) {
+}
+
+func TestReddit(t *testing.T) {
+}
